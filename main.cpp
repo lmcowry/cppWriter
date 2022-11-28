212a5a3815b2e597f6ce810e1a20ba8ad7c8c989
lmcowry <lowrymchristian@gmail.com>
2022-11-28 22:46:36 +0000

    write random thing everytime

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
+
+#include <random>
 
 int main()
 {
-    std::cout << "hi there" << '\n';
+    // std::cout << "hi there" << '\n';
+    std::vector<std::string> words {"hello", "goodbye"};
+    std::string specificWord;
+
+    //from https://stackoverflow.com/questions/13445688/how-to-generate-a-random-number-in-c
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> randomNum(1,words.size()); // distribution in range [1, 2]
+
+    specificWord = words[randomNum(rng) - 1];
+    //from https://www.learncpp.com/cpp-tutorial/basic-file-io/
     std::ofstream outf{ "/home/samuel_sewall/cppWriter/Sample.txt", std::ios::app };
 
     if (!outf)
@@ -12,8 +25,7 @@ int main()
         return 1;
     }
 
-    outf << "This is line 5\n";
-    outf << "This is line 6\n";
-    std::cout << "bye" << '\n';
+    outf << specificWord << '\n';
+    // outf << "This is line 6\n";
     return 0;
 }
